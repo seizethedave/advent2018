adbdb74d8115b5124d83d7eabe7103cafc5070b5
David Grant <david.grant@mixpanel.com>
2018-12-20 10:42:39 -0800

    Pathfinding.

--- advent15a.py
+++ advent15a.py
@@ -1,3 +1,4 @@
+import heapq
 import itertools
 
 class Wall(object):
@@ -13,14 +14,46 @@ class Open(object):
 WALL = Wall()
 OPEN = Open()
 
-def reachable(_from, _to, grid):
+def reachable(source, dest, grid):
     return True
 
-def squares_in_range(x, y, grid):
-    return filter(
+def open_adjacent_squares(x, y, grid):
+    return itertools.ifilter(
         lambda (x_, y_): grid[y_][x_] is OPEN,
         [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
     )
+
+def closest_target(source, targets, grid):
+    x, y = source
+    fringe = [(0, (y, x))]
+    seen = set()
+    nearest_distance = None
+    nearest = set()
+
+    while fringe:
+        dist, (y, x) = heapq.heappop(fringe)
+        seen.add((y, x))
+
+        if nearest_distance is not None and dist > nearest_distance:
+            # Already found something closer. Don't bother.
+            continue
+
+        if (y, x) in targets:
+            if nearest_distance is None:
+                nearest_distance = dist
+                nearest.add((y, x))
+            elif nearest_distance == dist:
+                nearest.add((y, x))
+            # Cannot beat this distance by traveling through this target square.
+            continue
+
+        for (nextX, nextY) in open_adjacent_squares(x, y, grid):
+            if (nextY, nextX) not in seen:
+                item = (dist + 1, (nextY, nextX))
+                heapq.heappush(fringe, item)
+
+    # min(nearest) is the first one in there, reading-order wise.
+    return min(nearest)
 
 class Player(object):
     def __init__(self, x, y, grid):
@@ -37,11 +70,16 @@ class Player(object):
     def play_turn(self):
         def iter_reachable_squares():
             for target in self.get_targets():
-                for (x, y) in squares_in_range(target.x, target.y, self.grid):
+                for (x, y) in open_adjacent_squares(target.x, target.y, self.grid):
                     if reachable(x, y, self.grid):
-                        yield (x, y)
+                        yield (y, x)
 
-        print list(iter_reachable_squares())
+        in_range_squares = set(iter_reachable_squares())
+        closest_in_range = closest_target(
+            (self.x, self.y), in_range_squares, self.grid)
+        print (self.y, self.x), "->", (closest_in_range)
+
+        # Take one step toward the closest.
 
     def move(self):
         pass
@@ -98,7 +136,6 @@ def go():
     for player in iter_players(grid):
         print player
         player.play_turn()
-
 
 if __name__ == "__main__":
     go()
