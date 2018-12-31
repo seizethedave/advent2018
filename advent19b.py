7e24bf25d3230153bc66755c9c187fc4d1e02450
David Grant <david.grant@mixpanel.com>
2018-12-31 09:28:49 -0800

    i and other are symmetrical, so only need to look at one side, and only up to sqrt(target).

--- advent19b.py
+++ advent19b.py
@@ -1,10 +1,12 @@
+import math
+
 target = 10551428
 count = 0
 
-for i in xrange(1, target + 1):
+for i in xrange(1, int(math.ceil(math.sqrt(target))) + 1):
     other = target // i
     if i * other == target:
         print i, other
-        count += i
+        count += i + other
 
 print count
